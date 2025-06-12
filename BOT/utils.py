163aecd8a4ffa8aa5698001c91d8b18c1887264d
Larkin Dunlop (Work) <larkindunlop2@gmail.com>
2025-06-12 13:06:45 +1000

    - hotfix for the qrcode

--- BOT/utils.py
+++ BOT/utils.py
@@ -81,7 +81,7 @@ def getMessageData(msg):
 def catchQR(qrCode: str, asciiQR: str, attempt: int, urlCode: str):
     import qrcode_terminal
     """Catches a QR code and prints it to the console."""
-    qrcode_terminal.draw(qrCode, small=True)
+    qrcode_terminal.draw(qrCode, version=1)
     # for line in asciiQR.splitlines():
     #     print(line)
         
