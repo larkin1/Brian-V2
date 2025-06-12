b7da4e2e5eaf2caa6a860e3e54f9d391c412bfa5
Larkin Dunlop (Work) <larkindunlop2@gmail.com>
2025-06-12 16:03:27 +1000

    - Fixed !help

--- BOT/help.py
+++ BOT/help.py
@@ -2,12 +2,12 @@
 
 def help(data, client):
     """Sends a help message to the user."""
-    from RESOURCES.Commands import commands
+    from BOT.Commands import userCommands
 
     if data["text"].lower().strip() == "!help":
         commandList = "\n".join(
-            f"- `!{i}`: _{commands[i][2]}_" if len(commands[i]) > 2 and commands[i][2] 
-            else f"`!{i}`" for i in commands
+            f"- `!{i}`: _{userCommands[i][2]}_" if len(userCommands[i]) > 2 and userCommands[i][2] 
+            else f"`!{i}`" for i in userCommands
         )
 
         text = f"""*Hi, I'm Brian II (The second)*
@@ -22,6 +22,6 @@ To learn more about a command and how to use it, type:
         client.sendText(data['chatId'], text)
     else:
         command = data["text"].lower().removeprefix("!help ").strip()
-        if command in commands.keys():
-            for i in commands[command][1]:
+        if command in userCommands.keys():
+            for i in userCommands[command][1]:
                 client.sendText(data["chatId"], i)
