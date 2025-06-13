66cc6e3967cd8c7a506ccf78ec8ba240f91cbf93
Larkin Dunlop (Work) <larkindunlop2@gmail.com>
2025-06-13 13:46:22 +1000

    - latest version at the time of creating this verison.

--- BOT/testfuncts.py
+++ BOT/testfuncts.py
@@ -5,12 +5,11 @@ def test(data, client):
     try:
         from BOT.globals import main_loop
         loop = main_loop
-        message = client.sendText(data.get("chatId"), "test meddahe")
-        print(message.get("id"))
-        for i in range(10):
-            time.sleep(0.2)
+        message = client.sendText(data.get("chatId"), "Test Number: 0")
+        for i in range(1000):
+            time.sleep(0.01)
             future = asyncio.run_coroutine_threadsafe(
-                client.editMessage(message.get("id").removesuffix("_out"), f"Test Number: {str(i)}"),
+                client.editMessage(message.get("id").removesuffix("_out"), f"Test Number: {str(i+1)}"),
                 loop
             )
             future.result()
