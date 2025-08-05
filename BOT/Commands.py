ac5bfd0f45aed4bc48e415c70688a0045751a199
Larkin Dunlop <larkindunlop2@gmail.com>
2025-08-05 17:55:20 +1000

    - Debugging

--- BOT/Commands.py
+++ BOT/Commands.py
@@ -34,7 +34,7 @@ adminCommands.update({
     "ban": (adminCmds.ban, 
         ["Bans a user or chat from using the bot."],
         "Admin Only Command"),
-    "adminTest": (testfuncts.adminTest, 
+    "admintest": (testfuncts.adminTest, 
         ["Admin Only Command"],
         "Admin Only Command"),
 })
